82c6fb73e48abda274be81d087de73bdb1d7c3ae
remia <jere.eledia@gmail.com>
2024-02-10 17:54:10 +0800

    Linked List files

--- 234.palindrome-linked-list.py
+++ 234.palindrome-linked-list.py
@@ -14,5 +14,48 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         # Imagine if I could use two pointers
         # Traversing through nodes
+        '''
+        # Array Solution
+        arr = []
+        while head:
+            arr.append(head.val)
+            head = head.next
+
+        l, r = 0, len(arr) - 1
+        while l < r:
+            if arr[l] != arr[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+        '''
+        fast = head
+        slow = head
+         
+        # While fast is not null
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # Reverse the 2nd half
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev 
+            prev = slow
+            slow = tmp
+
+        # Check the palindrome
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+
+
+
+
 # @lc code=end
 
